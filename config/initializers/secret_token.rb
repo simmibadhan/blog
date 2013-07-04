0efda2d367edb15cd2ef6d46cc0be5ec8da82918
simmi.badhan <simmi@addvalsolutions.com>
2013-07-04 18:30:18 +0530

    First commit - used scaffold command for posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cb8335fa415138b3976ef78bf6625bafd68472c22b0e3e982441f23d8d10e154c4b6c7778d0e41e63e129b144c39fc73aa5c91be4529a6be2265309daad20e73'
